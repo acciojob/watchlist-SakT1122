8524a0b3f796c014416c9887abf4465fec88bbd1
SakT1122 <sak.978tirbhane@gmail.com>
2023-01-27 23:29:12 +0530

    second

--- src/main/java/com/driver/Director.java
+++ src/main/java/com/driver/Director.java
@@ -5,6 +5,14 @@ public class Director {
     private int numberOfMovies;
     private double imdbRating;
 
+    public Director() {
+    }
+    public Director(String name, int numberOfMovies, double imdbRating) {
+        this.name = name;
+        this.numberOfMovies = numberOfMovies;
+        this.imdbRating = imdbRating;
+    }
+
     public void setImdbRating(double imdbRating) {
         this.imdbRating = imdbRating;
     }
